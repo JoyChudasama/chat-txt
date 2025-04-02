57c657f1bd8c4b8032a7275907f0d90fbd7ad878
JoyChudasama <joychudasama11@gmail.com>
2025-04-01 21:56:37 -0300

    remove redudandant import

--- backend/app/db/firestore.py
+++ backend/app/db/firestore.py
@@ -1,5 +1,4 @@
 import os
 from google.cloud import firestore
-from langchain_google_firestore import FirestoreChatMessageHistory
 
 FIRESTORE_CLIENT = firestore.Client(project=os.environ.get("FIRESTORE_PROJECT_ID"))
